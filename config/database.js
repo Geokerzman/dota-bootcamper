eb8d50259870d80f74ab892fb04b69798e8d1dc5
bilan <bilan.developer@gmail.com>
2024-07-26 17:30:55 +0200

    update

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(
+    process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
